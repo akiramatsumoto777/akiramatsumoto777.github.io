f874480afd010d719bba5201333538fa01648450
akiramatsumoto777 <74726760+akiramatsumoto777@users.noreply.github.com>
2021-11-21 15:55:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ $(function() {
         captureFlg = 2;
     });
     $('#atach_camera_audio').click(async function() {
-        alert('case 24.');
+        alert('case 25.');
         const localVideo = document.querySelector("video");
 
         videoStream = await getCameraAudioStream();
